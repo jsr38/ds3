1ff87e869a6207c0eef3e83dee166546f298fce9
Jeremy Reeve <jsr@jsrsolutions.co.nz>
2012-02-13 19:32:34 +1300

    use java property java.library.path instead of LD_LIBRARY_PATH.  Remove LD_LIBRARY_PATH reference

--- src/main/scripts/start-ds3.dev.sh
+++ src/main/scripts/start-ds3.dev.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# need this because the Java VM isn't looking in the trusted /usr/lib location by default
-# for our locally compiled HDF5 library 
-export LD_LIBRARY_PATH=/jsr/dist/hdf-java/lib/linux
-
 #JAVA_OPTS="-Dlog4j.configuration=/jsr/dist/ds3/dev/config/log4j.config.dev.xml"
 # previous version not employing HDF Java Object
-JAVA_OPTS="-Djava.library.path=/jsr/dist/hdf-java-2.7/lib/linux -Dlog4j.debug=false -Dlog4j.configuration=ds3.log4j.config.dev.xml -cp target/ds3-1.0-SNAPSHOT-jar-with-dependencies.jar"
+JAVA_OPTS="-Djava.library.path=/jsr/dist/hdf-java/lib/linux -Dlog4j.debug=false -Dlog4j.configuration=ds3.log4j.config.dev.xml -cp target/ds3-1.0-SNAPSHOT-jar-with-dependencies.jar"
 
 #JAVA_OPTS="-Dlog4j.debug=false -Dlog4j.configuration=ds3.log4j.config.dev.xml -cp target/ds3-1.0-SNAPSHOT-jar-with-dependencies.jar"
 
