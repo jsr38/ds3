08ffe93788ad52a1be5bc37456a87b00a642a0d3
Jeremy Reeve <jsr@jsrsolutions.co.nz>
2012-04-02 18:34:53 -0500

    add email functionality

---
+++ src/main/java/nz/co/jsrsolutions/ds3/DataScraper3Constants.java
@@ -0,0 +1,27 @@
+/* -*- mode: java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+
+/*
+ * @(#)DataScraper3Constants.java
+ *
+ * Copyright (c) 2012 JSR Solutions Limited
+ * 4 Viridian Lane, Auckland, 0632.  New Zealand
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of JSR
+ * Solutions Limited. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with JSR Solutions Limited.
+ */
+
+package nz.co.jsrsolutions.ds3;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+
+class DataScraper3Constants {
+
+    //    public static final String DATASCRAPER3_EMAIL_SUBJECT
+
+}
